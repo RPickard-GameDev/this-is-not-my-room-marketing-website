046767157f6215da9355da9f39bdf2680e7d116e
RPickard-GameDev <robert.pickard@ymail.com>
2026-01-03 02:39:03 +0000

    removed further dummy cotent

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -2,27 +2,11 @@
 const base = process.env.VITE_BASE || ''
 
 export const headerMenu = [
-    { name: 'Theme Info', link: `/${base}/theme-info` },
-    { name: 'Our Team', link: `/${base}/team` },
-    { name: 'Blog', link: `/${base}/blog` },
-    { name: 'Style-Guide', link: `/${base}/style-guide`, showArrow: false,
-        children: [
-            { name: 'Typography', link: '/style-guide#typography' },
-            { name: 'Colors', link: '/style-guide#colors' },
-            { name: 'Links', link: '/style-guide#links' },
-            { name: 'Buttons', link: '/style-guide#buttons' },
-            { name: 'Forms', link: '/style-guide#forms' },
-            { name: 'Lists', link: '/style-guide#lists' },
-        ]
-    }
 ];
 
 export const footerMenu = [
-    { name: 'Style Guide', link: '/style-guide' },
 ];
 
 export const legalMenu = [
-    { name: 'Privacy Policy', link: '/legal/privacy-policy' },
-    { name: 'Terms of Service', link: '/legal/terms-of-service' }
 ];
 
